21d1886f870a54b4c486166a17a055eec651cbd5
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-07 10:14:17 +0700

    format code

--- scripts/content.js
+++ scripts/content.js
@@ -1,98 +1,93 @@
 const fillColor = (data) => {
-	let color = 0
-	for (let index = 0; index < data.length; index++) {
-		if (data[index].color == undefined) {
-			data[index].color = ((dataColor) => {
-				if (dataColor != undefined) {
-					return dataColor
-				}
-				return color++
-			})(
-				data.find(
-					(subject) =>
-						subject.MaMH == data[index].MaMH &&
-						subject.color != undefined,
-				)?.color,
-			)
-		}
-	}
+  let color = 0;
+  for (let index = 0; index < data.length; index++) {
+    if (data[index].color == undefined) {
+      data[index].color = ((dataColor) => {
+        if (dataColor != undefined) {
+          return dataColor;
+        }
+        return color++;
+      })(
+        data.find(
+          (subject) =>
+            subject.MaMH == data[index].MaMH && subject.color != undefined
+        )?.color
+      );
+    }
+  }
 
-	data.sort((a, b) => a.color - b.color)
+  data.sort((a, b) => a.color - b.color);
 
-	return data
-}
+  return data;
+};
 
 const getData = () => {
-	const table = Array.from(document.querySelectorAll('.body-table tr'))
-	let data = []
-	const Thu = {
-		Hai: 2,
-		Ba: 3,
-		Tư: 4,
-		Năm: 5,
-		Sáu: 6,
-		Bảy: 7,
-	}
-	table.forEach((element) => {
-		const subject = Array.from(element.querySelectorAll('td'))
-			.reduce((previous, current) => {
-				return previous + current.innerText + '|'
-			}, '')
-			.split('|')
+  const table = Array.from(document.querySelectorAll(".body-table tr"));
+  let data = [];
+  const Thu = {
+    Hai: 2,
+    Ba: 3,
+    Tư: 4,
+    Năm: 5,
+    Sáu: 6,
+    Bảy: 7,
+  };
+  table.forEach((element) => {
+    const subject = Array.from(element.querySelectorAll("td"))
+      .reduce((previous, current) => {
+        return previous + current.innerText + "|";
+      }, "")
+      .split("|");
 
-		data.push({
-			MaMH: subject[0],
-			TenMH: subject[1],
-			NhomMH: subject[2],
-			STC: subject[3],
-			MaLop: subject[4],
-			STCHP: subject[5],
-			KDK: subject[6],
-			TH: subject[7],
-			Thu: Thu[subject[8]],
-			TietBD: parseInt(subject[9]),
-			ST: parseInt(subject[10]),
-			Phong: subject[11],
-			CBGV: subject[12],
-			Tuan: ((day) => {
-				return day
-					.split('--')
-					.map((day) => day.replace(/(\d+[/])(\d+[/])/, '$2$1'))
-					.map((date) => new Date(date))
-			})(
-				element
-					.querySelector('td:nth-child(14)')
-					.innerHTML.split("'")[1],
-			),
-		})
-	})
-	data = fillColor(data)
-	return data
-}
-
+    data.push({
+      MaMH: subject[0],
+      TenMH: subject[1],
+      NhomMH: subject[2],
+      STC: subject[3],
+      MaLop: subject[4],
+      STCHP: subject[5],
+      KDK: subject[6],
+      TH: subject[7],
+      Thu: Thu[subject[8]],
+      TietBD: parseInt(subject[9]),
+      ST: parseInt(subject[10]),
+      Phong: subject[11],
+      CBGV: subject[12],
+      Tuan: ((day) => {
+        return day
+          .split("--")
+          .map((day) => day.replace(/(\d+[/])(\d+[/])/, "$2$1"))
+          .map((date) => new Date(date));
+      })(element.querySelector("td:nth-child(14)").innerHTML.split("'")[1]),
+    });
+  });
+  data = fillColor(data);
+  return data;
+};
+console.log(getData());
 const renderButton = () => {
-	const button = document.createElement('button')
-	button.textContent = 'Cập nhật thời khóa biểu'
-	button.addEventListener('click', () => {
-		const data = getData()
-		chrome.storage.local.set({ tkb: JSON.stringify(data) }).then(() => {
-			alert('Đã cập nhật thời khóa biểu!')
-		})
-	})
-	document
-		.querySelector(
-			'#ctl00_ContentPlaceHolder1_ctl00_pnlHeader > table > tbody > tr:nth-child(1) > td',
-		)
-		.appendChild(button)
-}
+  const button = document.createElement("button");
+  button.textContent = "Cập nhật thời khóa biểu";
+  button.addEventListener("click", () => {
+    const data = getData();
+    chrome.storage.local.set({ tkb: JSON.stringify(data) }).then(() => {
+      alert("Đã cập nhật thời khóa biểu!");
+    });
+  });
+  document
+    .querySelector(
+      "#ctl00_ContentPlaceHolder1_ctl00_pnlHeader > table > tbody > tr:nth-child(1) > td"
+    )
+    .appendChild(button);
+};
 
 const autoStart = () => {
-	const button = document.querySelector(
-		'#ctl00_ContentPlaceHolder1_ctl00_rad_ThuTiet',
-	)
-	if (button && button.checked) {
-		renderButton()
-	}
-}
+  const button = document.querySelector(
+    "#ctl00_ContentPlaceHolder1_ctl00_rad_ThuTiet"
+  );
+  if (button && button.checked) {
+    renderButton();
+  }
+};
 
-autoStart()
+autoStart();
--- scripts/view.js
+++ scripts/view.js
@@ -1,25 +1,18 @@
-import { renderHTML } from './render.js'
+import { renderHTML } from "./render.js";
 
-const table = document.querySelector('.tkb-table')
-const styleEditor = document.querySelector('#css-style')
-const style = document.querySelector('#style')
-const save = document.querySelector('#save')
+const table = document.querySelector(".tkb-table");
+const styleEditor = document.querySelector("#css-style");
+const style = document.querySelector("#style");
+const save = document.querySelector("#save");
 
-await chrome.storage.local.get('css').then((data) => {
-	console.log(data['css'])
-	if (!data['css']) {
-		style.innerHTML = data['css']
-	}
-})
+table.innerHTML += await renderHTML();
 
-table.innerHTML += await renderHTML()
+styleEditor.innerHTML = style.innerHTML;
 
-styleEditor.innerHTML = style.innerHTML
+styleEditor.addEventListener("change", (event) => {
+  style.innerHTML = event.target.value;
+});
 
-styleEditor.addEventListener('change', (event) => {
-	style.innerHTML = event.target.value
-})
-
-save.addEventListener('click', () => {
-	chrome.storage.local.set({ css: styleEditor.innerHTML })
-})
+save.addEventListener("click", () => {
+  chrome.storage.local.set({ css: styleEditor.innerHTML });
+});
