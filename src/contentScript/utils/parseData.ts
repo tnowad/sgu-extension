598b53fe32eeec693cc605a3c29834ce4e1ca505
Nguyen Minh Tuan <tnowad@gmail.com>
2023-04-11 15:04:00 +0700

    add for english

--- src/contentScript/utils/parseData.ts
+++ src/contentScript/utils/parseData.ts
@@ -7,6 +7,12 @@ export const parseDayOfWeek = (day: string): number => {
     Năm: 5,
     Sáu: 6,
     Bảy: 7,
+    Mon: 2,
+    Tue: 3,
+    Wed: 4,
+    Thu: 5,
+    Fri: 6,
+    Sat: 7,
   };
   return daysOfWeek[day];
 };
